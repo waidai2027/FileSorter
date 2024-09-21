c82fbfd398aef9011fa06cc5dbaf17602dd1104a
huntert1004 <daringswordfights@gmail.com>
2024-09-21 15:49:21 -0400

    cleaned uo code

--- fileorganizer.py
+++ fileorganizer.py
@@ -1,53 +1,37 @@
 import os
-from pathlib import Path
 import logging
 import shutil
+from pathlib import Path
 
+# Define paths
 filespath = "C:/Users/hunte/Desktop/learn/categories/automation/randomfiles/"
+subdirectories_path = "C:/Users/hunte/Desktop/learn/categories/automation"
 
+# Get list of files in the source directory
+files_list = os.listdir(filespath)
 
-subdirecectoriespath = "C:/Users/hunte/Desktop/learn/categories/automation"
+# File category mappings
+file_categories = {
+    'images': ['.jpg', '.png'],
+    'documents': ['.pdf'],
+    'videos': ['.mp4', '.mov'],
+    'audios': ['.wav', '.mp3']
+}
 
-filespath_list = os.listdir(filespath)
-dir_list = os.walk(subdirecectoriespath)
-for x in filespath_list:
-    if x.endswith(".jpg") or x.endswith(".png"):
-        
-        if not os.path.isdir(subdirecectoriespath+"/images"):
-            os.makedirs(subdirecectoriespath+"/images")
-        shutil.move(filespath+x, subdirecectoriespath+"/images/"+x)
-        logging.info(filespath+x+" moved to "+subdirecectoriespath+"/images")
-
-
-
-        pass
-    elif x.endswith(".pdf"):
-        
-        if not os.path.isdir(subdirecectoriespath+"/documents"):
-            os.makedirs(subdirecectoriespath+"/documents")
-        shutil.move(filespath+x, subdirecectoriespath+"/documents/"+x)
-        logging.info(x+" moved to "+subdirecectoriespath+"/documents")
-        pass
-    elif x.endswith(".mp4") or x.endswith(".mov"):
-    
-        if not os.path.isdir(subdirecectoriespath+"/videos"):
-            os.makedirs(subdirecectoriespath+"/videos")
-        shutil.move(filespath+x, subdirecectoriespath+"/videos/"+x)
-        logging.info(filespath+x+" moved to "+subdirecectoriespath+"/videos")
-        pass
-    elif x.endswith(".wav") or x.endswith(".mp3"):
-        
-        if not os.path.isdir(subdirecectoriespath+"/audios"):
-            os.makedirs(subdirecectoriespath+"/audios")
-        shutil.move(filespath+x, subdirecectoriespath+"/audios/"+x)
-        logging.info(x+" moved to "+subdirecectoriespath+"/audios")
-        pass
+# Iterate through files
+for file in files_list:
+    # Identify the file type and move it to the appropriate subdirectory
+    for category, extensions in file_categories.items():
+        if any(file.endswith(ext) for ext in extensions):
+            destination_dir = os.path.join(subdirectories_path, category)
+            
+            # Create the subdirectory if it doesn't exist
+            if not os.path.isdir(destination_dir):
+                os.makedirs(destination_dir)
+            
+            # Move the file
+            shutil.move(os.path.join(filespath, file), os.path.join(destination_dir, file))
+            logging.info(f"{file} moved to {destination_dir}")
+            break
     else:
-        print("There are no files in the filespath variable")
-        break
-    
-
-
-    
-
-        
+        print(f"No matching file type for: {file}")
